26f532bf7234d715d59f30734f64b9eab53fc051
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-01 13:12:21 -0400

    Two Sum Solution Added

--- src/main/scala/leetcode/functional/programming/TwoSum.scala
+++ src/main/scala/leetcode/functional/programming/TwoSum.scala
@@ -4,6 +4,24 @@ object TwoSum {
 
   def main(args: Array[String]): Unit = {
 
-  }
+    val solution = new Solution()
 
+    println(solution.twoSum(Array(2, 3, 4, 5), 9).mkString("Array(", ", ", ")"))
+
+  }
+}
+
+class Solution {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+
+    val zipIndex = nums.zipWithIndex
+
+    val lookupMap = zipIndex.toMap
+
+    zipIndex
+      .collectFirst {
+        case (value, index) if lookupMap.get(target - value).exists(_ != index) =>
+          Array(index, lookupMap(target - value))
+      }.getOrElse(Array())
+  }
 }
