d6528413c6f1b90e54f407b1f979a20ed246a011
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-02 00:04:48 -0400

    Group Anagrams

--- src/main/scala/leetcode/functional/programming/GroupAnagrams.scala
+++ src/main/scala/leetcode/functional/programming/GroupAnagrams.scala
@@ -3,7 +3,15 @@ package leetcode.functional.programming
 object GroupAnagrams {
 
   def main(args: Array[String]): Unit = {
-    
+
+    val solution = new Solution()
+    println(solution.groupAnagrams(Array("eat", "tea", "tan", "ate", "nat", "bat")))
+  }
+
+  class Solution {
+    def groupAnagrams(strs: Array[String]): List[List[String]] = {
+      strs.groupBy(_.sorted).map(f => f._2.toList).toList
+    }
   }
 
 }
